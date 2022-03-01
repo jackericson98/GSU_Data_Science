aff9601079c7906be2596adeb21447b8d1757ba0
jackericson98 <jackericson98@gmail.com>
2022-03-01 14:42:04 -0500

    Cleaned up shape class

--- DSCI1302/Midterm/part_1b_shape_class.py
+++ DSCI1302/Midterm/part_1b_shape_class.py
@@ -1,6 +1,7 @@
+import numpy as np
+
 
 class Shape:
-
     """Class Used to contain subclasses"""
 
     def __init__(self):
@@ -11,38 +12,37 @@ class Shape:
 
 
 class Triangle(Shape):
-
-    """Parameters: {Side 1, Side 2, Angle between them}. Used to create
+    """Parameters: {Side 1, Side 2, Angle between side 1 and side 2}. Used to create
     an SAS triangle object."""
 
     def __init__(self, side1, side2, angle):
-
+        super().__init__()
         self.side1 = side1
         self.side2 = side2
         self.angle3 = angle
 
     def getVals(self):
-
-        """Used to assign all other side and angle values for our SAS triangle"""
+        """Used to assign all other side and angle values for our SAS triangle
+        Prints all values once they are assigned"""
 
         a = self.side1
         b = self.side2
         C = self.angle3
-        c = np.sqrt( a* * 2 + b* * 2 - 2 * a * b *np.cos(C))
-        A = np.arcsin(( a /c ) *np.sin(C))
-        B = 180 - ( A +C)
+        c = np.sqrt(a ** 2 + b ** 2 - 2 * a * b * np.cos(C))
+        A = np.arcsin((a / c) * np.sin(C))
+        B = 180 - (A + C)
         self.side3, self.angle1, self.angle2 = c, A, B
+        print("Your triangle:\nSides: {}, {}, {}\nAngles: {}, {}, {}".format(a, b, c, A, B, C))
+
 
     def area(self):
-
         """Used to calculate, assign, print and return the area for our triangle"""
 
-        self.area = 0. 5 *self.side 1 *self.side2
+        self.area = 0.5 * self.side1 * self.side2
         print(self.area)
         return self.area
 
     def perimeter(self):
-
         """Used to calculate, assign, print and return the perimeter for our triangle"""
 
         a, b, c = self.side1, self.side2, self.side3
@@ -51,31 +51,25 @@ class Triangle(Shape):
         return self.perimeter
 
 
-
 class Rectangle(Shape):
-
     """Parameters: {Side 1, Side 2, Angle between them}. Used to create
-    an SAS triangle object."""
+    a rectangle object."""
 
     def __init__(self, height, width):
+        super().__init__()
         self.height = height
         self.width = width
+        self.area = self.height * self.width
+        self.perimeter = 2 * (self.height + self.width)
 
     def area(self):
+        """Used to print and return the area for our rectangle"""
 
-        """Used to calculate, assign, print and return the area for our triangle"""
-
-        self.area = self.heigh t *self.width
         print(self.area)
         return self.area
 
     def perimeter(self):
+        """Used to print and return the perimeter for our rectangle"""
 
-        """Used to calculate, assign, print and return the perimeter for our triangle"""
-
-        self.perimeter = 2* (self.height + self.width)
         print(self.perimeter)
-        return (self.perimeter)
-
-
-
+        return self.perimeter
