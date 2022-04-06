c4e1d770d8bc265ce5d4f1046b374507085a2600
jackericson98 <jackericson98@gmail.com>
2022-04-05 20:19:23 -0400

    added choices method

--- MATH3020/FinalProject.py
+++ MATH3020/FinalProject.py
@@ -22,12 +22,11 @@ class Simulation:
         self.infected_list = []
 
     # Define our plot method. Since it does not call on the object we declare it as a static method
-    @staticmethod
-    def plot(num_sims, avg, sim_length_arr, avg_arr):
+    def plot(self, avg, sim_length_arr, avg_arr):
 
         # Plot the number of days for each simulation to run
-        plt.scatter(np.linspace(1, num_sims, num_sims), sim_length_arr)
-        plt.plot(np.linspace(1, num_sims, num_sims), avg_arr, color='r', linewidth=2.0)
+        plt.scatter(np.linspace(1, self.num_sims, self.num_sims), sim_length_arr)
+        plt.plot(np.linspace(1, self.num_sims, self.num_sims), avg_arr, color='r', linewidth=2.0)
 
         # Plot attributes
         plt.title("Days to clear network of viruses. Average = %3.5f" % avg)
@@ -41,41 +40,66 @@ class Simulation:
     # Define our choices method
     def choices(self):
 
+        input_found = False
+
         # Ask the user if they want to make any changes with the simulation
-        changes = input("Which of the following would you like to change:\n"
-                        "1. Number of computers    2. Infected computers    3. Infection Rate    "
-                        "4. Number of simulations\n").lower()
+        choices = input("\n Default settings: \n    1. Number of computers = {}\n    2. Initially infected computers"
+                        " = {}\n    3. Infection Rate = {}\n    4. Number of simulations = {}\n    5. Number of "
+                        "computers repaired daily = {}\n\nWould you like to change any of the above settings? If so, "
+                        "which setting(s)? (use numbers separated by commas or 'all') Type 'n' if not.\n\n"
+                        .format(self.num_comps, self.init_infected, self.infect_prob, self.num_sims,
+                                self.max_cure_pd)).lower()  #
+
+        if choices == 'n':
+            input_found = True
+
+        elif choices == 'y':
+            choices = input("Which setting(s)? (use numbers separated by commas or 'all')\n")
 
         # Create a list of changes from users input string
-        my_changes = changes.split(', ')
+        choice_list = choices.split(',')  # Splits by space comma space, comma space or comma
 
         # Input pools for each
-        num_comps = ["number of computers", '1.', '1', 'computers']
-        infected_computers = ['infected computers', 'number infected', 'infected', '2.', '2']
-        infection_rate = ['infection rate', 'infection', '3', '3.']
-        num_sims = ["number of simulations", 'num sims', '4.', '4']
+        num_comps_keys = ['1.', '1', ' 1.', ' 1', 'all']
+        infected_computers_keys = ['2.', '2', ' 2.', ' 2', 'all']
+        infection_rate_keys = ['3', '3.', ' 3.', ' 3', 'all']
+        num_sims_keys = ['4.', '4', ' 4.', ' 4', 'all']
+        mc_pd_keys = ['5', '5.', ' 5.', ' 5', 'all']
 
         # Check if user indicated to change the number of computers
-        if any(x in num_comps for x in my_changes):
-            self.num_comps = int(input("How many computers would you like to simulate?"))
+        if any(x in num_comps_keys for x in choice_list):
+            input_found = True
+            self.num_comps = int(input("1. How many computers would you like to simulate?\n"))
 
         # Check if user indicated to change the number of infected computers
-        if any(x in infected_computers for x in my_changes):
-            self.init_infected = int(input("How many infected computers would you like to start with?"))
+        if any(x in infected_computers_keys for x in choice_list):
+            input_found = True
+            self.init_infected = int(input("2. How many infected computers would you like to start with?\n"))
 
         # Check if user indicated to change the infection rate
-        if any(x in infection_rate for x in my_changes):
-            self.infect_prob = float(input("What is the infection rate you would like to test?"))
+        if any(x in infection_rate_keys for x in choice_list):
+            input_found = True
+            self.infect_prob = float(input("3. What is the infection rate you would like to test?\n"))
 
         # Check if user indicated to change the number of simulations
-        if any(x in num_sims for x in my_changes):
-            self.num_sims = int(input("How many simulations would you like to run?"))
+        if any(x in num_sims_keys for x in choice_list):
+            input_found = True
+            self.num_sims = int(input("4. How many simulations would you like to run?\n"))
+
+        # Check if user indicated to change the number of repaired computers per day
+        if any(x in mc_pd_keys for x in choice_list):
+            input_found = True
+            self.max_cure_pd = int(input("5. How many repaired computers per day?\n"))
+
+        # Catch all other
+        if not input_found:
+            print("\nSorry \"{}\" is not a valid input. Please try again. \n".format(choices))
+            self.choices()
 
     # Method to simulate a day
     def day(self):
 
         # Infection process:
-
         # Go through the infected computer list
         for i in range(len(self.infected_list)):
 
@@ -96,7 +120,6 @@ class Simulation:
 
         # Keep curing computers until we reach the desired number of cured computers or all computers are healthy
         while cured <= self.max_cure_pd and len(self.infected_list) > 0:
-
             # Choose a random index from our infected computer list, remove that index (pop) and cure the
             # corresponding computer (i.e. set it's value to False)
             self.comp_list[self.infected_list.pop(np.random.randint(len(self.infected_list)))] = False
@@ -107,13 +130,7 @@ class Simulation:
     # Method for running the simulation
     def run(self):
 
-        choices = input("\n1. Number of computers = {}\n2. Initially infected computers = {}\n3. Infection Rate = {}\n"
-                        "4. Number of simulations = {}\n5. Number of computers repaired daily = {}\n\n"
-                        "Would you like to change any of the above settings for your simulation? (y/n):"
-                        .format(self.num_comps, self.init_infected, self.infect_prob, self.num_sims, self.max_cure_pd))
-
-        if choices.lower() != 'n':
-            self.choices()
+        self.choices()
 
         # Set our average back to 0 and reset our data arrays
         avg = 0
@@ -123,16 +140,13 @@ class Simulation:
         # Run the number of simulations specified
         for i in range(self.num_sims):
 
-            # Reset our lists for each simulation
+            # Reset our lists and day counter for each simulation
             self.comp_list = [True] * self.init_infected + [False] * (self.num_comps - self.init_infected)
             self.infected_list = list(range(0, self.init_infected, 1))
-
-            # Set the day counter to 0
             days = 0
 
             # Keep running days until all computers are cured
-            while len(self.infected_list) > 0:
-
+            while len(self.infected_list) > 0 and days <= 1000000:
                 # Call the day method and add a day to our counter
                 self.day()
                 days += 1
@@ -147,7 +161,12 @@ class Simulation:
                   end='')
 
         # Plot the results
-        self.plot(self.num_sims, avg, sim_length_arr, avg_arr)
+        self.plot(avg, sim_length_arr, avg_arr)
+
+        # Ask the user if they would like to run another simulation.
+        cont = input("\n\nWould you like to run another simulation?\n")
+        if cont.lower() == 'y':
+            self.run()
 
 
 # Driver code
